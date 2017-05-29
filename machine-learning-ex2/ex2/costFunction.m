58db36e410cfd9bf618c1359eed9e5b539d73d17
Jain <vikas.e.jain@accenture.com>
2017-05-29 14:25:42 +0530

    Week 3 exercises

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,10 +19,11 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h= sigmoid(theta'*X)
 
+J = (1/m)*((-y'*log(h)) - ((1-y)'*log(1-h)))
 
-
-
+grad = theta - (1/m)*X'((-y'*log(h)) - ((1-y)'*log(1-h)))
 
 
 
